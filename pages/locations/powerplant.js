e53bbc920bd6550f87cb285bcc86bddbdd4bf9ca
danstepanov <danhstepanov@gmail.com>
2022-01-02 01:49:14 -0800

    initial commit

---
+++ pages/locations/powerplant.js
@@ -0,0 +1,19 @@
+import { useRouter } from 'next/router'
+
+export default function PowerPlant() {
+    const router = useRouter()
+
+    return (
+        <div className="flex flex-col items-start h-screen w-screen bg-black">
+            <button className='bg-yellow-500 font-black p-2 m-2' onClick={() => router.back()}>{`< Back`}</button>
+            <text className="font-black p-2 m-2 text-white text-2xl">Power Plant</text>
+            <div className="flex-1">
+                <img
+                    className='h-11/12'
+                    src={`/powerplant.jpeg`}
+                    alt=""
+                />
+            </div>
+        </div>
+    )
+}
---
+++ pages/locations/village.js
@@ -0,0 +1,19 @@
+import { useRouter } from 'next/router'
+
+export default function Village() {
+    const router = useRouter()
+
+    return (
+        <div className="flex flex-col items-start h-screen w-screen bg-black">
+            <button className='bg-yellow-500 font-black p-2 m-2' onClick={() => router.back()}>{`< Back`}</button>
+            <text className="font-black p-2 m-2 text-white text-2xl">Village</text>
+            <div className="flex-1">
+                <img
+                    className='h-11/12'
+                    src={`/village.jpeg`}
+                    alt=""
+                />
+            </div>
+        </div>
+    )
+}
