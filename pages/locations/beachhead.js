09e072812eedefea1761e18a75771d317a18120e
danstepanov <danhstepanov@gmail.com>
2022-01-02 01:55:13 -0800

    copy fix again

--- pages/locations/beachhead.js
+++ pages/locations/beachhead.js
@@ -6,7 +6,7 @@ export default function BeachHead() {
     return (
         <div className="flex flex-col items-start h-screen w-screen bg-black">
             <button className='bg-yellow-500 font-black p-2 m-2' onClick={() => router.back()}>{`< Back`}</button>
-            <text className="font-black p-2 m-2 text-white text-2xl">Beach Head</text>
+            <text className="font-black p-2 m-2 text-white text-2xl">Beachhead</text>
             <div className="flex-1">
                 <img
                     className='h-11/12'
